c5a8fdf7d2bff08401326634d07743a39dfb2374
PMagombedze <percymagoras@outlook.com>
2023-05-24 22:49:41 +0200

    s_shell

--- main.h
+++ main.h
@@ -7,7 +7,5 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-#define MAX_INPUT 80 /* Maximum length of command */
-#define MAX_ARGS 10 /* Maximum number of arguments */
 
 #endif /* MAIN_H */
--- s_shell.c
+++ s_shell.c
@@ -1,50 +1,19 @@
 #include "main.h"
+
 /**
  * main - main function
+ *
  * Return: always 0
  */
 int main(void)
 {
-char *args[MAX_ARGS], cmd[MAX_INPUT], *tkn;
-int loop = 1, i = 0;
-pid_t pid;
-while (loop)
+char command[256];
+int loop = 1;
+while (loop) /* start of infinite loop */
 {
-printf("($) ");
-fflush(stdout);
-fgets(cmd, MAX_INPUT, stdin);
-if (cmd[strlen(cmd) - 1] == '\n')
-{
-cmd[strlen(cmd) - 1] = '\0';
-}
-tkn = strtok(cmd, " ");
-for (i = 0; tkn != NULL; i++)
-{
-args[i] = tkn;
-tkn = strtok(NULL, " ");
-}
-args[i] = NULL;
-if (strcmp(args[0], "exit") == 0)
-{
-loop = 0;
-break;
-}
-pid = fork();
-if (pid == 0)
-{
-execvp(args[0], args);
-perror("./shell: No such file or directory");
-exit(EXIT_FAILURE);
-}
-else if (pid < 0)
-{
-perror("fork");
-exit(EXIT_FAILURE);
-}
-else
-{
-wait(NULL);
-}
+printf("$ ");   /* prompt to user */
+fgets(command, 256, stdin); /* collect input command*/
+system(command); /* run entered command */
 }
 return (0);
 }
